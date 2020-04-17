572deaf00a4b44dc33a5bfffb46239a37a7c47c8
James Batsell <jamesbatsell05@gmail.com>
2020-04-17 23:52:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ class App < Sinatra::Base
   end
 
   get "/goodbye/:name" do
-    @user_name = params[:name]
-    "Goodbye, #{@user_name}!"
+    @name = params[:names]
+    "Goodbye, #{@name}!"
   end
 
 end
